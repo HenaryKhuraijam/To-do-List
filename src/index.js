c0ae82535c939dd665e0df017245c08dc9d6302a
sami <samif3443@gmail.com>
2023-04-15 07:14:33 -0700

    Fix everything

--- src/index.js
+++ src/index.js
@@ -1,45 +1,13 @@
-// import './style.css';
-import {
-  addTodo, render, removeTodo, edit,
-} from './modules/functions.js';
+import Tasks from './tasks.js';
 
-// ADD A NEW TASK
-const addButton = document.querySelector('.addBtn'); // clicking add button
-addButton.addEventListener('click', () => {
-  const addTask = document.querySelector('.addInput');
-  add(addTask);
+const tasks = new Tasks();
+const addTaskBtn = document.getElementById('addBtn');
+
+addTaskBtn.addEventListener('click', () => {
+  tasks.addTask();
 });
 
-const addTask = document.querySelector('.addInput'); // typing enter key
-addTask.addEventListener('keydown', (event) => {
-  if (event.keyCode === 13) {
-    const addTask = document.querySelector('.addInput').value;
-    add(addTask);
-    render();
-  }
+const clearBtn = document.getElementById('clearBtn');
+clearBtn.addEventListener('click', () => {
+  tasks.clearComplete();
 });
-
-// DELETE A TASK
-const todoContainer = document.querySelector('.todoContainer');
-
-todoContainer.addEventListener('click', (event) => {
-  const deleteTaskIcon = event.target.closest('.delete-task-icon');
-  if (deleteTaskIcon) {
-    const deleteTaskIcons = todoContainer.querySelectorAll('.delete-task-icon');
-    const index = Array.from(deleteTaskIcons).indexOf(deleteTaskIcon);
-    remove(index);
-  }
-});
-
-// EDIT A TASK
-todoContainer.addEventListener('click', (event) => {
-  const textInput = event.target.closest('.input-text');
-  if (textInput) {
-    const textInputs = todoContainer.querySelectorAll('.input-text');
-    const index = Array.from(textInputs).indexOf(textInput);
-    edit(index);
-  }
-});
-
-// FIRST RENDER WHEN THE PAGE LOADS
-window.onload = render;
